8b603cfbce43b6a3e3d898e0f71e7b507b81c80a
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-22 17:31:28 -0800

    apply testing for the spots for DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,10 +11,20 @@ export default function DayListItem(props) {
     "day-list__item--full": spots === 0,
   });
 
+  const formatSpots = () => {
+    if (spots === 0) {
+      return `no spots `;
+    } else if (spots === 1) {
+      return `1 spot `;
+    } else {
+      return `${spots} spots `;
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
