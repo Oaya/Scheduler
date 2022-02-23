b67a16bbe8e09d5f9c3ea33a7b3ec91abda1120c
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-22 16:53:43 -0800

    apply style for DatListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,20 @@
 import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+  let dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
     </li>
   );
 }
