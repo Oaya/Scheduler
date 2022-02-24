562c90f253bd49e31ac47d27cdaa7f346e4ecd16
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-23 21:21:28 -0800

    refactor and add storybook for Appointment Booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,22 @@ import React from "react";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
-import Confirm from "./Confirm";
-import Status from "./Status";
-import Error from "./Error";
+// import Confirm from "./Confirm";
+// import Status from "./Status";
+// import Error from "./Error";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+  const { time, interview } = props;
   return (
     <article className="appointment">
-      {props.time ? `Appointment at ${props.time}` : `No Appointments`}
+      <Header time={time} />
+      {interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
     </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -133,6 +133,7 @@ storiesOf("Appointment", module)
   .add("Show", () => (
     <Show
       student="Lydia Miller-Jones"
+      interviewer={interviewer}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />
@@ -163,4 +164,20 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
