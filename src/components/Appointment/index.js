9025bdb5a3d8fde80eef807ae022ddc3aea6f9b2
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-23 16:26:15 -0800

    refactor and add storybook for Empty.js Header.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Header from "./Header";
+import Empty from "./Empty";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
