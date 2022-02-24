9d354805a3f23d116dc4068a8d673f555f2658af
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-23 16:57:58 -0800

    refactor and add storybook for Confirm.js

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,19 @@
+import Button from "components/Button";
+import React from "react";
+
+export default function Confirm(props) {
+  const { message, onConfirm, onCancel } = props;
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">{message}</h1>
+      <section className="appointment_-actions">
+        <Button danger onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={onConfirm}>
+          Confirm
+        </Button>
+      </section>
+    </main>
+  );
+}
