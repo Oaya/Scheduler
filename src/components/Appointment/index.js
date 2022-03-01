22677779d3bc53fe82a0dfcb97b47753e13b7f3b
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-02-28 16:41:56 -0800

    add transition for deleting appointment' ;

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,8 +5,7 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import Form from "./Form";
-
-// import Confirm from "./Confirm";
+import Confirm from "./Confirm";
 // import Status from "./Status";
 // import Error from "./Error";
 
@@ -19,6 +18,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING"
 const DELETING = 'DELETING'
+const CONFIRM = 'CONFIRM';
 
 export default function Appointment({
   id,
@@ -30,12 +30,11 @@ export default function Appointment({
 }) {
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
-  function cancel() {
-
+  function onCancel() {
     back();
   }
 
-  function save(name, interviewer) {
+  function onSave(name, interviewer) {
     transition(SAVING)
     const interview = {
       student: name,
@@ -51,9 +50,13 @@ export default function Appointment({
     transition(DELETING)
     cancelInterview(id)
       .then(() => {
+
         transition(EMPTY)
       })
 
+  }
+  function onTransition() {
+    transition(CONFIRM)
   }
 
   return (
@@ -61,17 +64,18 @@ export default function Appointment({
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show student={interview.student} interviewer={interview.interviewer} onDelete={onDelete} />
+        <Show student={interview.student} interviewer={interview.interviewer} onDelete={onTransition} />
       )}
       {mode === CREATE && (
         <Form
           interviewers={interviewers}
-          onCancel={cancel}
-          onSave={save}
+          onCancel={onCancel}
+          onSave={onSave}
         />
       )}
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && <Confirm message="Are you sure you would like to delete?" onCancel={onCancel} onConfirm={onDelete} />}
     </article>
   );
 }
