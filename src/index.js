f03069ca276bcde345fadaa9d01e0a86fef79bdd
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-03 22:17:03 -0800

    add requirement for netrify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL =
+    process.env.REACT_APP_API_BASE_URL;
+}
+
+ReactDOM.render(
+  <Application />,
+  document.getElementById("root")
+);
